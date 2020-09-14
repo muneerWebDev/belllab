8c98a03ea9ba9b055190e9c91142f94687bdc7c7
Muhammed Muneer <muneer@2hatslogic.com>
2020-09-14 12:22:24 +0530

    coming soon

--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,57 @@
 $(document).ready(function () {
+
     AOS.init();
-    //page loading effect
-    setTimeout(function () {
-        $(".init-loader").addClass("loaded");
-    }, 800);
 
-    setTimeout(function () {
-        $("body").css({
-            "overflow": 'auto',
-            "height": "auto"
-        });
-    }, 3000);
+    function dynamicFunctions() {
+
+        //getting dimensions
+        var headerHeight = $(".main-header").height();
+        var containerMargin = parseInt($(".main-header .container").css("margin-right"));
+
+        //defining css variables
+        $("body").get(0).style.setProperty("--navbar-height", headerHeight + 'px');
+        $("body").get(0).style.setProperty("--containerMargin", containerMargin + 'px');
 
 
-    //getting dimensions
-    var headerHeight = $(".main-header").height();
-    var containerMargin = parseInt($(".main-header .container").css("margin-right")) + 15;
+        //page loading effect
+        setTimeout(function () {
+            $(".init-loader").addClass("loaded");
+        }, 800);
 
-    //defining css variables
-    $("body").get(0).style.setProperty("--navbar-height", headerHeight + 'px');
+        setTimeout(function () {
+            $("body").css({
+                "overflow": 'auto',
+                "height": "auto"
+            });
+        }, 3000);
 
-    //home-banner
-    $(".banner .parallax-bg").css({
-        "margin-right": "-" + containerMargin + 'px',
-        "margin-left": "-" + (containerMargin * 1.5) + 'px'
-    })
+        //home-banner
+        if ($("window").width() > 1450) {
+            $(".banner .parallax-bg").css({
+                "margin-right": "-" + (containerMargin + 15) + 'px',
+                "margin-left": "-" + ((containerMargin + 15) * .85) + 'px'
+            })
+
+        } else if ($(window).width() > 1200) {
+            $(".banner .parallax-bg").css({
+                "margin-right": "-" + (containerMargin + 15) + 'px',
+                "margin-left": "-" + (280) + 'px'
+            })
+        }
+
+        //slider custom container
+
+        $(".slider-container-left").css("margin-left", containerMargin);
+        $(".slider-container-right").css("margin-right", containerMargin);
+
+    }
+
+    dynamicFunctions();
+    $(window).resize(function () {
+        dynamicFunctions();
+    });
 
     //parallax
-
     $(window).scroll(function () {
         var scrollTop = $(window).scrollTop();
         // $(".banner .parallax-image.x1 img").css("transform", "translateY("+scrollTop*.1+"px)");
@@ -40,25 +64,53 @@ $(document).ready(function () {
             $("body").removeClass("scrolled");
         }
     })
-    // $('.banner').mousemove(function (e) {
-    //     var x = e.pageX - this.offsetLeft;
-    //     if ($('.banner .parallax-bg .bg').css('right') <= '400') {
-    //         $('.banner .parallax-bg .bg').css({
-    //             'right': x
-    //         });
-    //         console.log(x);
-    //     }
-    // });
 
-    $('.banner').mousemove(function(e){
-        var valueX=(e.pageX * -1 / 100); 
-        var valueY=(e.pageY * -1 / 100); 
-         
+    $(".parallax-sec").mouseenter(function () {
+        $(this).addClass("hovered");
+    });
+
+    $(".parallax-sec").mouseleave(function () {
+        $(this).removeClass("hovered");
+    });
+
+    $('.banner').mousemove(function (e) {
+        var valueX = (e.pageX * -1 / 60);
+        var valueY = (e.pageY * -1 / 60);
+
         $('.banner .parallax-bg .bg').css({
-            'transform':'translate3d('+valueX+'px,'+valueY+'px,0)'
+            'transform': 'translate3d(' + valueX + 'px,' + valueY + 'px,0)'
         });
-        // $('.banner .parallax-bg .main-g').css({
-        //      'transform':'translate3d('+(valueX/2)+'px,'+(valueX/2)+'px,0)'
-        //  });
-     });
+
+        $('.banner .collegues .main-img-wrap, .banner .collegues .papers-wrap').css({
+            'transform': 'translate3d(' + (-valueX / 2) + 'px,' + (-valueY / 2) + 'px,0)'
+        });
+    });
+
+    $(".services .slider-wrapper").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.arrows-wrap .left'),
+        nextArrow: $('.arrows-wrap .right'),
+        responsive: [
+            {
+              breakpoint: 767,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 575,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    })
+
 });
